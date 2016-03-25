5a464cdc22f2f7241bd8075aeab6287fbb380a8b
Tooler-87 <gavinot87@hotmail.com>
2016-03-25 16:29:47 +0000

    Update Pseudo_Loc.js

--- Pseudo_Loc.js
+++ Pseudo_Loc.js
@@ -1,30 +1,43 @@
-//-----Pseudo_Loc_Generic-----
-var arr = ['div', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'b', 'i', 'p', 'span', 'u', 'li', 'tr'];
-var arr_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; // Spaces added to avoid appended text appearing as one long word.
-
-for (var i = 0; i < arr.length; i++) {
-    $('body ' + arr[i]).each(function (index) {
-        var txt = $(this).contents().filter(function () {
+javascript: (function() {
+  function callback() {
+    (function($) {
+      var jQuery = $;
+      var arr = ['div', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'b', 'i', 'p', 'span', 'u', 'li', 'tr'];
+      var arr_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; // Spaces added to avoid appended text appearing as one long word.
+      for (var i = 0; i < arr.length; i++) {
+        $('body ' + arr[i]).each(function(index) {
+          var txt = $(this).contents().filter(function() {
             return this.nodeType == 3;
-        }).text();
-        if (/\S/.test(txt)) {
+          }).text();
+          if (/\S/.test(txt)) {
             var tag = $(this).html();
             txt = txt.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
             if (txt.length < 3) {} else {
-                var result = txt;
-                var extra_lth = (result.length / 100) * 30; //Increase to suit.
-                extra_lth = Math.round(extra_lth);
-                var extra = '';
-                for (var x = 0; x < extra_lth; x++) {
-                    extra += arr_alphabet[Math.floor(Math.random() * arr_alphabet.length)];
-                }
-                var re = new RegExp(txt, 'g');
-                var updated = tag.replace(re, result);
-                var add = '[' + extra + ']';
-                $(this).contents().filter(function () {
-                    return this.nodeType == 3;
-                })[0].nodeValue += add;
+              var result = txt;
+              var extra_lth = (result.length / 100) * 30; //Increase to suit.
+              extra_lth = Math.round(extra_lth);
+              var extra = '';
+              for (var x = 0; x < extra_lth; x++) {
+                extra += arr_alphabet[Math.floor(Math.random() * arr_alphabet.length)];
+              }
+              var re = new RegExp(txt, 'g');
+              var updated = tag.replace(re, result);
+              var add = '[' + extra + ']';
+              $(this).contents().filter(function() {
+                return this.nodeType == 3;
+              })[0].nodeValue += add;
             }
-        }
-    });
-}
+          }
+        });
+      }
+    })(jQuery.noConflict(true))
+  }
+  var s = document.createElement("script");
+  s.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js";
+  if (s.addEventListener) {
+    s.addEventListener("load", callback, false)
+  } else if (s.readyState) {
+    s.onreadystatechange = callback
+  }
+  document.body.appendChild(s);
+})()
